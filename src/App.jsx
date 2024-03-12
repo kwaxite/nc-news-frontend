fbc5d18aab0c2e39525d83d44348f8e2881442b6
kwaxite <146014461+kwaxite@users.noreply.github.com>
2024-03-12 14:13:57 +0000

    View an individual article

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import ArticlesList from './components/ArticlesList/ArticlesList';
@@ -8,16 +8,16 @@ import SingleArticle from './components/SingleArticle/SingleArticle';
 
 
 function App() {
- 
+
 
   return (
     <>
-    <Header/>
-    <Routes>
-    <Route path='/' element={<Home/>}></Route>
-    <Route path='/articles' element={<ArticlesList/>}></Route>
-    <Route path='/articles/:article_id' element={<SingleArticle/>}></Route>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/articles' element={<ArticlesList />}></Route>
+        <Route path='/articles/:article_id' element={<SingleArticle />}></Route>
+      </Routes>
     </>
   )
 }
--- src/components/SingleArticle/SingleArticle.jsx
+++ src/components/SingleArticle/SingleArticle.jsx
@@ -2,27 +2,36 @@ import { useParams } from "react-router-dom"
 import { fetchArticleById } from "../../Api"
 import { useState } from "react"
 import { useEffect } from "react"
-export default function SingleArticle(){
-    const {article_id} = useParams()
+import "../SingleArticle/SingleArticle.css";
+
+export default function SingleArticle() {
+    const { article_id } = useParams()
     const [article, setArticle] = useState({})
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         setIsLoading(true);
         fetchArticleById(article_id).then((articleFromApi) => {
-          setArticle(articleFromApi);
-          setIsLoading(false);
+            setArticle(articleFromApi);
+            setIsLoading(false);
         });
-      }, []);
-    
-      if (isLoading) return <p>Loading...</p>;
+    }, []);
+
+    if (isLoading) return <p>Loading...</p>;
     return (
         <>
-        <h1>Single Article</h1>
-        <h2>{article.title}</h2>
+            <div className="single-article">
+
+                <h1>{article.title}</h1>
+                <h3>By {article.author}</h3>
+                <img className="article-image" src={article.article_img_url} />
+                <p>{article.body}</p>
+                <p> Created at: {article.created_at}</p>
+                <p>Votes: {article.votes}</p>
+                <p>Topic: {article.topic}</p>
+            </div>
 
         </>
     )
 }
 
-// fetchArticleById(1)
